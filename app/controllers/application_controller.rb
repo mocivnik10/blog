b2087468b5425a4585133e590df953cc0770cf3d
mocivnik10 <iztok.mocivnik@poviolabs.com>
2016-04-06 19:00:04 +0200

    Modified project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
-    if admin_signed_in?
+    if user_signed_in?
       articles_path
     else
       session[:previous_url] || root_path
--- db/schema.rb
+++ db/schema.rb
@@ -11,25 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160226220210) do
+ActiveRecord::Schema.define(version: 20160226220156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "admins", force: :cascade do |t|
-    t.string   "email",              default: "", null: false
-    t.string   "encrypted_password", default: "", null: false
-    t.integer  "sign_in_count",      default: 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.integer  "failed_attempts",    default: 0
-    t.string   "unlock_token"
-    t.datetime "locked_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
