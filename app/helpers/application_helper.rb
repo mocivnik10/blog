3c3d8d4e271cd1f53f5f508d1533f545d7bcf515
mocivnik10 <iztok.mocivnik@poviolabs.com>
2016-04-15 12:36:50 +0200

    Helper for comment numbers, fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def my_comment_helper(article)
+    if article.comments.count === 1
+      return "komentar"
+    elsif article.comments.count === 2
+      return "komentarja"
+    elsif article.comments.count === 3 || article.comments.count === 4
+      return "komentarji"
+    else
+      return "komentarjev"
+    end
+  end
+
 end
