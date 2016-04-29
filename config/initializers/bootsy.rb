171d6fd161013c5a8d1d0cefd0a1b4c643642e9e
mocivnik10 <iztok.mocivnik@poviolabs.com>
2016-04-29 14:25:57 +0200

    Bootsy images upload to s3 on production

--- config/initializers/bootsy.rb
+++ config/initializers/bootsy.rb
@@ -55,7 +55,11 @@ Bootsy.setup do |config|
   #   'fog' to your Gemfile and create and configure your credentials in an
   #   initializer file, as described in Carrierwave's docs:
   #   https://github.com/carrierwaveuploader/carrierwave#using-amazon-s3
-  # config.storage = :file
+  if Rails.env.production?
+    config.storage = :fog
+  else
+    config.storage = :file
+  end
   #
   #
   # Store directory (inside 'public') for storage = :file
