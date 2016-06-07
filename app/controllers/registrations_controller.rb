9281398cc5cfc7f12ee7e275de1466373c3c5cd4
mocivnik10 <iztok.mocivnik@poviolabs.com>
2016-06-07 13:52:35 +0200

    user can edit his basic profiles

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,18 @@
 class RegistrationsController < Devise::RegistrationsController
+  before_action :set_user, only: [:edit, :update]
 
+
+  def edit
+  end
+
+  def update
+    if @user.update(account_update_params)
+      sign_in(@user, bypass: true)
+      redirect_to articles_path, flash: {notice: "Uporabnik uspešno posodobljen!"}
+    else
+      render 'edit'
+    end
+  end
 
   private
 
@@ -9,5 +22,9 @@ class RegistrationsController < Devise::RegistrationsController
 
   def account_update_params
     params.require(:user).permit(:nickname, :email, :password, :password_confirmation, :current_password)
+  end
+
+  def set_user
+    @user = current_user
   end
 end
--- app/helpers/registrations_helper.rb
+++ app/helpers/registrations_helper.rb
@@ -1,2 +1,10 @@
 module RegistrationsHelper
+
+  def devise_error_messages!
+    if resource.errors.full_messages.any?
+        flash.now[:error] = resource.errors.full_messages.join(' & ')
+    end
+    return ''
+  end
+
 end
